8daedd2e383d717a1ced705d7bd024b0ba52ec7b
colo <msanchez.jose.94@gmail.com>
2018-10-30 21:19:50 -0300

    getAllChannels implementado en UnquitubeService

--- webapp/app/services/UnquiTubeService.js
+++ webapp/app/services/UnquiTubeService.js
@@ -13,28 +13,6 @@ function UnquiTubeService($http,$rootScope) {
                 errorCallback && errorCallback(error);
             }
         );
-        /*
-        // HARDCODEADO para pruebas ********************************************************************
-        return [
-            new Video("Escape Room Trailer #1 (2019) | Movieclips Trailers", "https://www.youtube.com/embed/8W6yYBAUxv4", ""),
-            new Video("Aquaman TV Spot - Waves (2018) | Movieclips Trailers", "https://www.youtube.com/embed/nmGmu-knybU", ""),
-            new Video("The Curse of La Llorona Teaser Trailer #1 (2019) | Movieclips Trailers", "https://www.youtube.com/embed/MNp1LPm3Krs", ""),
-            new Video("Aladdin Teaser Trailer #1 (2019) | Movieclips Trailers", "https://www.youtube.com/embed/TzWlGGokGeE", ""),
-            new Video("Glass Trailer #2 (2019) | Movieclips Trailers", "https://www.youtube.com/embed/oAALE9m47dc", ""),
-            new Video("Jonathan Trailer #1 (2018) | Movieclips Trailers", "https://www.youtube.com/embed/V17pEDh6VYw", ""),
-            new Video("The Best of Chucky Mashup | Movieclips", "https://www.youtube.com/embed/QQCv4rxlHVg", ""), 
-            new Video("In Theaters Now: Halloween | Weekend Ticket", "https://www.youtube.com/embed/9gWsyEkKO1A", ""), 
-            new Video("Jamie Lee Curtis on The Legacy of Halloween’s Laurie Strode - Part 2", "https://www.youtube.com/embed/dvbePi1o_Q0", ""), 
-            new Video("Five Favorite Horror Films: Jason Blum | Rotten Tomatoes", "https://www.youtube.com/embed/v6p6ftyb_nM", ""), 
-            new Video("Horror Stars Reveal Their Scariest Movies of All Time | Rotten Tomatoes", "https://www.youtube.com/embed/lLMiu_xWS70", ""), 
-            new Video("Brenton Thwaites on New 'Titans' Robin and Breaking Up With Batman | Rotten Tomatoes", "https://www.youtube.com/embed/xKWY3umXJZU", ""),
-            new Video("Halloween Trailer #1 (2018) | Movieclips Trailers", "https://www.youtube.com/embed/aMCLVSlk1Tk", ""), 
-            new Video("The Hate U Give Trailer #1 (2018) | Movieclips Trailers", "https://www.youtube.com/embed/1nl9If0lAtY", ""), 
-            new Video("Wildlife Teaser Trailer #1 (2018) | Movieclips Trailers", "https://www.youtube.com/embed/QQDL4stQCqo", ""), 
-            new Video("Mid90s Trailer #1 (2018) | Movieclips Trailers", "https://www.youtube.com/embed/zXzSE0uqmjI", ""), 
-            new Video("Can You Ever Forgive Me? Trailer #1 (2018) | Movieclips Trailers", "https://www.youtube.com/embed/OVZ1u36nrns", ""), 
-            new Video("What They Had Trailer #1 (2018) | Movieclips Trailers", "https://www.youtube.com/embed/AoEsF02gnUQ", ""),
-        ];*/
     };
 
     self.saveVideo = function (videoToSave, successCallback, errorCallback) {
--- webapp/app/views/home/home.js
+++ webapp/app/views/home/home.js
@@ -5,7 +5,7 @@ app.config(function($routeProvider) {
     $routeProvider
         .when("/", {
             templateUrl : "./app/views/home/home.html",
-            controller: ["$scope", "$log", HomeCtrl],
+            controller: ["$scope", "$log","UnquiTubeService", HomeCtrl],
             controllerAs: "homeCtrl"
         }
     );
@@ -15,8 +15,23 @@ app.config(function($routeProvider) {
 /**
  * Controller de la página home
  */
-function HomeCtrl($scope, $log) {
+function HomeCtrl($scope, $log,UnquiTubeService) {
 
-    
+    const self = this;
+    self.channels = null;
 
+    UnquiTubeService.getAllChannels(
+        function(response) {
+            self.playlist = response.data;
+        },
+        function(error) {
+            window.alert("Ha sucedido un error al obtener los canales");
+            console.error(error);
+        } 
+
+    );        
 }
+
+
+
+
